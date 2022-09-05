cbb7e64694b5a83a8ff86c90f72bb2cf08683e5e
Evelyn Pai <wowitsevelynwow+github@gmail.com>
2022-09-05 16:22:00 -0700

    prefix type

--- operators/groups.py
+++ operators/groups.py
@@ -2,42 +2,42 @@ from typing import Set
 
 from sushi_cleanups.operators.sushi_base_operator import SushiBaseOperator
 
-from .delete_empty_bone_groups import (
+from .delete_armature_empty_bone_groups import (
     SUSHI_CLEANUP_DeleteEmptyBoneGroupsAll,
     SUSHI_CLEANUP_DeleteEmptyBoneGroupsSelected,
 )
-from .delete_empty_collections import SUSHI_CLEANUP_DeleteEmptyCollections
-from .delete_empty_uv_maps import (
+from .delete_global_empty_collections import SUSHI_CLEANUP_DeleteEmptyCollections
+from .delete_global_unused_armatures import SUSHI_CLEANUP_DeleteUnusedArmatures
+from .delete_global_unused_materials import SUSHI_CLEANUP_DeleteUnusedMaterials
+from .delete_global_unused_meshes import SUSHI_CLEANUP_DeleteUnusedMeshes
+from .delete_mesh_empty_uv_maps import (
     SUSHI_CLEANUP_DeleteEmptyUVMapsAll,
     SUSHI_CLEANUP_DeleteEmptyUVMapsSelected,
 )
-from .delete_empty_vertex_colors import (
+from .delete_mesh_empty_vertex_colors import (
     SUSHI_CLEANUP_DeleteEmptyVertexColorsAll,
     SUSHI_CLEANUP_DeleteEmptyVertexColorsSelected,
 )
-from .delete_empty_vertex_groups import (
+from .delete_mesh_empty_vertex_groups import (
     SUSHI_CLEANUP_DeleteEmptyVertexGroupsAll,
     SUSHI_CLEANUP_DeleteEmptyVertexGroupsSelected,
 )
-from .delete_unused_armatures import SUSHI_CLEANUP_DeleteUnusedArmatures
-from .delete_unused_material_slots import (
+from .delete_mesh_unused_material_slots import (
     SUSHI_CLEANUP_DeleteUnusedMaterialSlotsAll,
     SUSHI_CLEANUP_DeleteUnusedMaterialSlotsSelected,
 )
-from .delete_unused_materials import SUSHI_CLEANUP_DeleteUnusedMaterials
-from .delete_unused_meshes import SUSHI_CLEANUP_DeleteUnusedMeshes
-from .delete_unused_uv_maps import (
+from .delete_mesh_unused_uv_maps import (
     SUSHI_CLEANUP_DeleteUnusedUVMapsAll,
     SUSHI_CLEANUP_DeleteUnusedUVMapsSelected,
 )
-from .delete_unused_vertex_colors import (
+from .delete_mesh_unused_vertex_colors import (
     SUSHI_CLEANUP_DeleteUnusedVertexColorsAll,
     SUSHI_CLEANUP_DeleteUnusedVertexColorsSelected,
 )
-from .rename_unique_armatures import SUSHI_CLEANUP_RenameUniqueArmatures
-from .rename_unique_meshes import SUSHI_CLEANUP_RenameUniqueMeshes
-from .rename_unique_user_materials import SUSHI_CLEANUP_RenameUniqueUserMaterials
-from .sort_all_vertex_groups import SUSHI_CLEANUP_SortVertexGroups
+from .rename_global_unique_armatures import SUSHI_CLEANUP_RenameUniqueArmatures
+from .rename_global_unique_meshes import SUSHI_CLEANUP_RenameUniqueMeshes
+from .rename_global_unique_user_materials import SUSHI_CLEANUP_RenameUniqueUserMaterials
+from .sort_global_all_vertex_groups import SUSHI_CLEANUP_SortVertexGroups
 
 ALL_OPERATIONS: Set[SushiBaseOperator] = {
     SUSHI_CLEANUP_DeleteEmptyBoneGroupsAll,
@@ -64,11 +64,11 @@ ALL_OPERATIONS: Set[SushiBaseOperator] = {
     SUSHI_CLEANUP_DeleteUnusedVertexColorsSelected,
 }
 
-DELETE_ALL: Set[SushiBaseOperator] = {
+DELETE_MESH_ALL: Set[SushiBaseOperator] = {
     x for x in ALL_OPERATIONS if "ALL" in x.sk_tags and "DELETE" in x.sk_tags
 }
 
-DELETE_SELECTED: Set[SushiBaseOperator] = {
+DELETE_MESH_SELECTED: Set[SushiBaseOperator] = {
     x for x in ALL_OPERATIONS if "SELECTED" in x.sk_tags and "DELETE" in x.sk_tags
 }
 
